cdd7571b62dbf41c51504f21e931e258c28200ce
Fighter <Fighter@DESKTOP-NEP6U65>
2023-04-14 15:26:33 +0430

    Add project files.

---
+++ mahdiyar aghaei 679/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mahdiyar_aghaei_679
+{
+    internal class post { 
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set;}
+        public int useId { get; set; }
+
+        StreamWriter writer = new StreamWriter("user.txt");
+foreach(var postList in posts)
+{
+    if (postList.userId % 2 != 0)
+    {
+        writer.WriteLine($"user id : {postList.userId}, Title: {postList.title}");
+    }
+}
+writer.Close();
